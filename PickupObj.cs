007fbc5237bff17f2bacfc914f6aeff0a33fb4e6
PartumGames <37095216+PartumGames@users.noreply.github.com>
2018-06-14 14:38:09 -0500

    Add files via upload

---
+++ PickupObj.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PickupObj : MonoBehaviour {
+
+    public Transform hand;
+    Rigidbody rb;
+    public Item myItem;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.isKinematic = false;
+    }
+
+
+    public void Pickup()
+    {
+        rb.useGravity = false;
+        rb.isKinematic = true;
+        transform.position = hand.position;
+        transform.rotation = hand.rotation;
+        transform.parent = hand;
+    }
+
+    public void Drop()
+    {
+        rb.useGravity = true;
+        rb.isKinematic = false;
+        transform.parent = null;
+    }
+
+}
---
+++ Player.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public Camera cam;
+    public Transform hand;
+    public float interactDist = 10f;
+
+    public GameObject objImHolding;
+    public Item myItem;
+    public bool isholding;
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Drop();
+        }
+
+        DoInteraction();
+    }
+
+    private void DoInteraction()
+    {
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray, out hit, interactDist))
+        {
+            if(hit.collider.tag == "Harvest")
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    Harvest currentHarvest = hit.collider.GetComponent<Harvest>();
+                    currentHarvest.HarvestResources(myItem.itemAttackDamage);
+                }
+            }
+
+
+            if(hit.collider.tag == "Pickup")
+            {
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    PickupObject(hit.collider.gameObject);
+                }
+            }
+
+            
+        }
+    }
+
+    private void PickupObject(GameObject objToPickup)
+    {
+        if (isholding)
+        {
+            Drop();
+        }
+
+        objImHolding = objToPickup;
+        objImHolding.GetComponent<PickupObj>().Pickup();
+        myItem = objImHolding.GetComponent<PickupObj>().myItem;
+        isholding = true;
+    }
+
+    private void Drop()
+    {
+        objImHolding.GetComponent<PickupObj>().Drop();
+        objImHolding = null;
+        myItem = null;
+        isholding = false;
+    }
+
+
+    //private void Attack(TestDummy go)
+    //{
+    //    if (itemImHolding != null)
+    //    {
+    //        go.TakeDamage(myItem.itemAttackDamage);
+    //    }
+    //    else
+    //    {
+    //        print("You need a weapon to attack");
+    //    }
+    //}
+}
